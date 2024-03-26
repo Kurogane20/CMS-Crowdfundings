2d77ff78d4d31c97d276ae44a42283e6de7d73e3
Kurogane20 <nurkhalis4@gmail.com>
2024-03-26 17:20:06 +0700

    perbaikan tampilan

--- resources/views/public/home.blade.php
+++ resources/views/public/home.blade.php
@@ -94,7 +94,7 @@
 
                 <div class="row">
                     <div class="col-md-12">
-                        <h2 class="section-title"> Ayo Donasi </h2>
+                        <h2 class="section-title" style="font-weight: bold" > Ayo Donasi </h2>
                     </div>
                 </div>
 
